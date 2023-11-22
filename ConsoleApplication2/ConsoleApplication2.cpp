7a14c0f9956d923645059d4ea495e75d720143b2
13-4 <13-4@DESKTOP-FJCRVH6>
2023-11-22 13:03:55 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,29 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+	srand(time(NULL));
+	int random[15];	int min = 0;
+	int minLim; cout << "enter Min Limit for numbers  = "; cin >> minLim;
+	int maxLim; cout << "enter Max Limt for numbers = "; cin >> maxLim;
+	for (int i = 0; i < 15; i++)
+	{
+		random[i] = rand()%(maxLim - minLim+1)+minLim;
+		cout << random[i] << "  "; 
+
+        if (min > random[i])
+		{
+			min = random[i];
+		}
+
+	}
+
+	
+	cout << endl << min;
+}
+
+
